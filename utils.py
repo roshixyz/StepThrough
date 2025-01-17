c6c014394e97c9809b8c44b02821e44f8a08c543
Pranay <khaireaman14@gmail.com>
2025-01-17 10:00:39 +0530

    clean utils.py

--- utils.py
+++ utils.py
@@ -2,94 +2,24 @@ import ast
 import copy
 import inspect
 import json
-import os
-import sys
 from itertools import chain
 
 from cheap_repr import cheap_repr, try_register_repr
 
 from expressions import ExpExec
 
-PYPY = 'pypy' in sys.version.lower()
-NO_BIRDSEYE = PYPY
-
-pp_name_prefix = '__deep_pp_hidden__'
-
 file_reading_errors = (
     IOError,
     OSError,
-    ValueError  # IronPython weirdness.
+    ValueError
 )
-
-
-def shitcode(s):
-    return ''.join(
-        (c if (0 < ord(c) < 256) else '?') for c in s
-    )
-
-
-def truncate(seq, max_length, middle):
-    if len(seq) > max_length:
-        left = (max_length - len(middle)) // 2
-        right = max_length - len(middle) - left
-        seq = seq[:left] + middle + seq[-right:]
-    return seq
-
-
-def truncate_string(string, max_length):
-    return truncate(string, max_length, '...')
-
-
-def truncate_list(lst, max_length):
-    return truncate(lst, max_length, ['...'])
-
-
-def ensure_tuple(x, split=False):
-    if split and isinstance(x, str):
-        x = x.replace(',', ' ').split()
-    if not isinstance(x, (list, set, tuple)):
-        x = (x,)
-    return tuple(x)
-
-
-def short_filename(code):
-    result = os.path.basename(code.co_filename)
-    if result.endswith('.pyc'):
-        result = result[:-1]
-    return result
-
 
 def is_comprehension_frame(frame):
     return frame.f_code.co_name in ('<listcomp>', '<dictcomp>', '<setcomp>')
 
 
-def needs_parentheses(source):
-    def code(s):
-        return compile(s.format(source), '<variable>', 'eval').co_code
-
-    try:
-        without_parens = code('{}.x')
-    except SyntaxError:
-        # Likely a multiline expression that needs parentheses to be valid
-        code('({})')
-        return True
-    else:
-        return without_parens != code('({}).x')
-
-
-def with_needed_parentheses(source):
-    if needs_parentheses(source):
-        return '({})'.format(source)
-    else:
-        return source
-
 
 REPR_TARGET_LENGTH = 100
-
-
-def my_cheap_repr(x):
-    return cheap_repr(x, target_length=REPR_TARGET_LENGTH)
-
 
 class ArgDefaultDict(dict):
     def __init__(self, factory):
@@ -106,19 +36,6 @@ def optional_numeric_label(i, lst):
         return ''
     else:
         return ' ' + str(i + 1)
-
-
-def is_pathlike(x):
-    if hasattr(os, 'PathLike'):
-        return isinstance(x, os.PathLike)
-
-    return (
-            hasattr(x, '__fspath__') or
-            # Make a concession for older `pathlib` versions:
-            (hasattr(x, 'open') and
-             'path' in x.__class__.__name__.lower())
-    )
-
 
 def process_logs(log_data):
     
@@ -207,12 +124,6 @@ except Exception:
 
 def no_args_decorator(args, kwargs):
     return len(args) == 1 and inspect.isfunction(args[0]) and not kwargs
-
-
-class DirectRepr(str):
-    def __repr__(self):
-        return self
-
 
 def _register_cheap_reprs():
     def _sample_indices(length, max_length):
